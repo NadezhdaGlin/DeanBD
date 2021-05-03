1a725b9c215a7e55dfece44f374e5596d3caed5b
Nadezhda <glinovanad@gmail.com>
2021-05-03 15:52:49 +0300

    db connection

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const express = require("express")
 const apiRouter = require("./apiRoutes");
+const mysql = require("mysql2");
+
+  
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "dean_db",
+  password: ""
+});
+ connection.connect(function(err){
+    if (err) {
+      return console.error("Ошибка: " + err.message);
+    }
+    else{
+      console.log("Подключение к серверу MySQL успешно установлено");
+    }
+ });
+
 
 var app = express();
 app.use("/api", apiRouter);
 
 app.get('/', (req, res) => //req - запрос, res - ответ
-{
-    console.log("Кто-то стучит в api");
-    res.setHeader('Access-Control-Allow-Origin', '*');
+        
+{   res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
-    res.send("Hello {req} API");
+    connection.query("SELECT * FROM students LIMIT 5", (err, results, fields) => {
+        console.log(err);
+        res.send(results); // собственно данные
+        console.log(fields); // мета-данные полей 
+});
+ 
+    console.log("Кто-то стучит в api");
 });
 
 app.listen(3012, () =>
