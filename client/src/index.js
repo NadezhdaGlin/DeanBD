46cede1a8a4a8c9aa77b91ddb53bf74c1627a375
Nadezhda <glinovanad@gmail.com>
2021-05-04 18:38:45 +0300

    немного css и datagrid

--- client/src/index.js
+++ client/src/index.js
@@ -7,5 +7,16 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+//    document.getElementById('root'),
+//    document.getElementById('article'),
+    document.getElementById('header')
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+//    document.getElementById('root'),
+//    document.getElementById('article'),
+    document.getElementById('header')
 );
--- client/src/pages/testButton.js
+++ client/src/pages/testButton.js
@@ -1,22 +1,36 @@
-function name() {
-    fetch("http://127.0.0.1:3012/", {
-        method: "GET",
-        origin: "http://127.0.0.1:3012/"
-    })
-    .then(res => res.text())
-    .then(resText => {
-        document.getElementById("smt").innerText = resText;
-    });
+import React from 'react';
+import ReactDataGrid from 'react-data-grid';
+
+const columns = [
+  { key: 'id', name: 'ID' },
+  { key: 'title', name: 'Title' },
+  { key: 'count', name: 'Count' } ];
+
+const rows = [{id: 0, title: 'row1', count: 20}, {id: 1, title: 'row1', count: 40}, {id: 2, title: 'row1', count: 60}];
+
+function HelloWorld() {
+  return (<ReactDataGrid
+  columns={columns}
+  rowGetter={i => rows[i]}
+  rowsCount={3}
+  minHeight={150} />);
 }
 
+//function name() {
+//    fetch("http://127.0.0.1:3012/", {
+//        method: "GET",
+//        origin: "http://127.0.0.1:3012/"
+//    })
+//    .then(res => res.text())
+//    .then(resText => {
+//        document.getElementById("smt").innerText = resText;
+//    });
+//}
+//
 export const TestButton = () => {
-    return (
-        <div>
-            <h1 id="smt">
-                Hello world!
-            </h1>
-
-            <button onClick={name}>Кнопка</button>
-        </div>
-    )
+  return (<ReactDataGrid
+  columns={columns}
+  rowGetter={i => rows[i]}
+  rowsCount={3}
+  minHeight={150} />);
 }
