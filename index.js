85a2b4a8ad7cc19fdb3a137c8e8536f28f6524d2
Nadezhda <glinovanad@gmail.com>
2021-05-10 20:17:30 +0300

    index.js

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
 const express = require("express")
 const apiRouter = require("./apiRoutes");
-const mysql = require("mysql2");
-
-  
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "dean_db",
-  password: ""
-});
- connection.connect(function(err){
-    if (err) {
-      return console.error("Ошибка: " + err.message);
-    }
-    else{
-      console.log("Подключение к серверу MySQL успешно установлено");
-    }
- });
-
 
 var app = express();
 app.use("/", apiRouter);
